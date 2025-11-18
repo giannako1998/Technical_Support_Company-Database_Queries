d250b63a5b04409836e22f7bc5c24ba413fee610
giannako1998 <g.giannako1998@gmail.com>
2025-11-18 22:08:23 +0200

    Update Key_Queries.sql

--- Key_Queries.sql
+++ Key_Queries.sql
@@ -1,6 +1,5 @@
--- Query 1: Εύρεση όλων των ανοιχτών (Open) ή σε εξέλιξη (In Progress) αιτημάτων, ταξινομημένων κατά υψηλή προτεραιότητα (High/Urgent),
--- ώστε να γνωρίζεις με τι πρέπει να ασχοληθείς άμεσα.
--
+-- Query 1: Εύρεση όλων των ανοιχτών (Open) ή σε εξέλιξη (In Progress) αιτημάτων, ταξινομημένων κατά υψηλή προτεραιότητα (High/Urgent), ώστε να γνωρίζεις με τι πρέπει να ασχοληθείς άμεσα.
+
 SELECT
     T.ticket_id,
     T.subject,
@@ -61,8 +60,7 @@ ORDER BY
     Closed_Tickets_Count DESC;
 
 
--- Query 4: Ενημέρωση κατάστασης ενός Ticket μετά την επίλυση
--- (Υποθέτουμε ότι επιλύθηκε το Ticket 4)
+-- Query 4: Διαχείριση Κατάστασης: Εντολή UPDATE για την αλλαγή της κατάστασης ενός Ticket σε 'Closed' μετά την επίλυση.
 
 UPDATE Support_Tickets
 SET
@@ -73,3 +71,31 @@ WHERE
 
 -- Έλεγχος ότι η ενημέρωση πέτυχε:
 SELECT ticket_id, subject, status, priority FROM Support_Tickets WHERE ticket_id = 4;
+
+
+-- Query 5: Εύρεση των 3 πελατών με τον μεγαλύτερο συνολικό όγκο Tickets
+
+SELECT
+    C.company_name AS Customer_Company,
+    COUNT(T.ticket_id) AS Total_Tickets_Submitted
+FROM
+    Support_Tickets T
+JOIN
+    Customers C ON T.customer_id = C.customer_id
+GROUP BY
+    C.company_name
+ORDER BY
+    Total_Tickets_Submitted DESC
+LIMIT 3;
+
+
+-- Query 6: Υπολογισμός του μέσου χρόνου επίλυσης (σε ημέρες) για τα κλειστά Tickets
+SELECT
+    AVG(DATEDIFF(NOW(), T.date_created)) AS Avg_Resolution_Days
+FROM
+    Support_Tickets T
+WHERE
+    T.status = 'Closed';
+
+
+
